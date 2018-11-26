3de9d77758da36703779cfff78f10db9e1598bc8
yatto0716 <kai_423@i.softbank.jp>
2018-11-26 15:55:49 +0900

    ハローワールドをふやしました

--- Sample.java
+++ Sample.java
@@ -1,5 +1,9 @@
 public class Sample {
 	public static void main(String[] args){
 		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
 	}
 }
